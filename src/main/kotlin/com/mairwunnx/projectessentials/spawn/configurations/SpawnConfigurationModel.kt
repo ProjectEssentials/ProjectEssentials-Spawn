1eea22504e0ad417d73a4fb76ebbee61bc187332
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-01 23:16:27 +0300

    Position now has integer type.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/spawn/configurations/SpawnConfigurationModel.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/spawn/configurations/SpawnConfigurationModel.kt
@@ -5,9 +5,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class SpawnConfigurationModel(
     var dimensionId: Int = 0,
-    var xPos: Double = 0.5,
-    var yPos: Double = 100.0,
-    var zPos: Double = 0.5,
+    var xPos: Int = 0,
+    var yPos: Int = 100,
+    var zPos: Int = 0,
     var yaw: Float = 0f,
     var pitch: Float = 0f
 )
