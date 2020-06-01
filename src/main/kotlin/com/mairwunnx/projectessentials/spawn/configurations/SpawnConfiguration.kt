71d9e1a9d69843d053acfebb7c562581535cac0a
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-01 21:16:19 +0300

    SpawnConfiguration.kt implemented.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/spawn/configurations/SpawnConfiguration.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/spawn/configurations/SpawnConfiguration.kt
@@ -1,4 +1,46 @@
 package com.mairwunnx.projectessentials.spawn.configurations
 
-object SpawnConfiguration {
+import com.mairwunnx.projectessentials.core.api.v1.configuration.IConfiguration
+import com.mairwunnx.projectessentials.core.api.v1.helpers.jsonInstance
+import com.mairwunnx.projectessentials.core.api.v1.helpers.projectConfigDirectory
+import org.apache.logging.log4j.LogManager
+import java.io.File
+import java.io.FileNotFoundException
+
+object SpawnConfiguration : IConfiguration<SpawnConfigurationModel> {
+    private val logger = LogManager.getLogger()
+    private var configurationData = SpawnConfigurationModel()
+
+    override val name = "spawn"
+    override val version = 1
+    override val configuration = take()
+    override val path = projectConfigDirectory + File.separator + "spawn.json"
+
+    override fun load() {
+        try {
+            val configRaw = File(path).readText()
+            configurationData = jsonInstance.parse(
+                SpawnConfigurationModel.serializer(), configRaw
+            )
+        } catch (ex: FileNotFoundException) {
+            logger.error("Configuration file ($path) not found!")
+            logger.warn("The default configuration will be used")
+        }
+    }
+
+    override fun save() {
+        File(path).parentFile.mkdirs()
+
+        logger.info("Saving configuration `${name}`")
+        val raw = jsonInstance.stringify(
+            SpawnConfigurationModel.serializer(), configuration
+        )
+        try {
+            File(path).writeText(raw)
+        } catch (ex: SecurityException) {
+            logger.error("An error occurred while saving $name configuration", ex)
+        }
+    }
+
+    override fun take() = configurationData
 }
